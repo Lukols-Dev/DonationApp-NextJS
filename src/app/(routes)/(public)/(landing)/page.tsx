0a760450a812fc82da0c5cd9e39ca6aec12f6cc2
Lukols-Dev <lukols.dev@gmail.com>
2024-03-05 22:32:33 +0100

    fix url

--- src/app/(routes)/(public)/(landing)/page.tsx
+++ src/app/(routes)/(public)/(landing)/page.tsx
@@ -8,7 +8,7 @@ import ImageSection from "@/components/sections/ImageSection";
 
 export default function Home() {
   return (
-    <main className="flex flex-col">
+    <main className="flex flex-col ">
       <Preloader />
       <HeroSection />
       <InfiniteText />
