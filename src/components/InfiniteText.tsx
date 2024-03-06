d3299af326913ba981190ae2005516d71fd46c33
Lukols-Dev <lukols.dev@gmail.com>
2024-03-06 13:24:26 +0100

    add fix animation

--- src/components/InfiniteText.tsx
+++ src/components/InfiniteText.tsx
@@ -1,4 +1,68 @@
+// "use client";
+// import { useLayoutEffect, useRef } from "react";
+// import gsap from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// const InfiniteText = () => {
+//   const firstText = useRef(null);
+//   const secondText = useRef(null);
+//   const slider = useRef(null);
+//   let xPercent = 0;
+//   let direction = -1;
+
+//   useLayoutEffect(() => {
+//     gsap.registerPlugin(ScrollTrigger);
+//     gsap.to(slider.current, {
+//       scrollTrigger: {
+//         trigger: document.documentElement,
+//         scrub: 0.25,
+//         start: 0,
+//         end: window.innerHeight,
+//         onUpdate: (e) => (direction = e.direction * -1),
+//       },
+//       x: "-500px",
+//     });
+//     requestAnimationFrame(animate);
+//   }, []);
+
+//   const animate = () => {
+//     if (xPercent < -100) {
+//       xPercent = 0;
+//     } else if (xPercent > 0) {
+//       xPercent = -100;
+//     }
+//     gsap.set(firstText.current, { xPercent: xPercent });
+//     gsap.set(secondText.current, { xPercent: xPercent });
+//     requestAnimationFrame(animate);
+//     xPercent += 0.1 * direction;
+//   };
+
+//   return (
+//     <div className="overflow-hidden relative flex -top-[50px]">
+//       <div className="h-[500px]">
+//         <div ref={slider} className="relative whitespace-nowrap">
+//           <p
+//             ref={firstText}
+//             className="relative m-0 text-black text-[230px] font-medium pr-[50px]"
+//           >
+//             Monetyzuj - Zarządzaj -
+//           </p>
+//           <p
+//             ref={secondText}
+//             className="absolute left-[100%] top-0 m-0 text-black text-[230px] font-medium pr-[50px]"
+//           >
+//             Monetyzuj - Zarządzaj -
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default InfiniteText;
+
 "use client";
+
 import { useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -7,35 +71,52 @@ const InfiniteText = () => {
   const firstText = useRef(null);
   const secondText = useRef(null);
   const slider = useRef(null);
-  let xPercent = 0;
-  let direction = -1;
 
   useLayoutEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
+
+    let xPercent = 0;
+    let direction = -1; // Teraz zmienna jest zdefiniowana w odpowiednim zakresie
+
+    // Sprawdzenie, czy referencje do elementów są dostępne
+    if (!slider.current || !firstText.current || !secondText.current) {
+      return; // Przerywa wykonanie, jeśli którykolwiek z elementów jest niezdefiniowany
+    }
+
+    // Definicja animacji
     gsap.to(slider.current, {
       scrollTrigger: {
         trigger: document.documentElement,
         scrub: 0.25,
         start: 0,
         end: window.innerHeight,
-        onUpdate: (e) => (direction = e.direction * -1),
+        onUpdate: (self) => {
+          direction = self.direction === -1 ? 1 : -1;
+        },
       },
       x: "-500px",
     });
-    requestAnimationFrame(animate);
-  }, []);
 
-  const animate = () => {
-    if (xPercent < -100) {
-      xPercent = 0;
-    } else if (xPercent > 0) {
-      xPercent = -100;
-    }
-    gsap.set(firstText.current, { xPercent: xPercent });
-    gsap.set(secondText.current, { xPercent: xPercent });
-    requestAnimationFrame(animate);
-    xPercent += 0.1 * direction;
-  };
+    const animate = () => {
+      if (!firstText.current || !secondText.current) {
+        return; // Przerywa wykonanie, jeśli którykolwiek z elementów jest niezdefiniowany
+      }
+
+      if (xPercent < -100) {
+        xPercent = 0;
+      } else if (xPercent > 0) {
+        xPercent = -100;
+      }
+
+      gsap.set(firstText.current, { xPercent: xPercent });
+      gsap.set(secondText.current, { xPercent: xPercent });
+      xPercent += 0.1 * direction;
+
+      requestAnimationFrame(animate);
+    };
+
+    requestAnimationFrame(animate); // Rozpoczęcie animacji
+  }, []);
 
   return (
     <div className="overflow-hidden relative flex -top-[50px]">
--- src/components/common/Buttons/LoginBtn.tsx
+++ src/components/common/Buttons/LoginBtn.tsx
@@ -4,6 +4,10 @@ import { cn } from "@/lib/utils";
 import Magnetic from "../Magnetic";
 import { generalSans } from "@/fonts";
 import { useLoginModal } from "@/hooks/useModal";
+import getCurrentUser from "@/lib/auth-actions";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { getSession } from "next-auth/react";
 // import { signOut, useSession } from "next-auth/react";
 
 interface Props {
@@ -13,32 +17,44 @@ interface Props {
 const font = generalSans.medium.className;
 
 const LoginBtn = ({ title }: Props) => {
-  // const { data: session } = useSession();
+  const [user, setUser] = useState<string>("");
+
+  const getUser = async () => {
+    const currentUser: any = await getSession();
+    if (currentUser && currentUser.user && currentUser.user.uid) {
+      setUser(currentUser.user.uid);
+    }
+  };
+
   const { onOpen } = useLoginModal();
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
     <Magnetic>
-      {/* {session && session.user ? (
-        <div
-          onClick={() => signOut()}
+      {user && user.length > 0 ? (
+        <Link
+          href={`${process.env.NEXT_PUBLIC_URL}/user`}
           className={cn(
             font,
             "flex items-center relative justify-center text-[#18181A] text-base px-8 py-4 rounded-full border border-[#18181A] cursor-pointer"
           )}
         >
-          WYLOGUJ SIĘ
+          Panel klienta
+        </Link>
+      ) : (
+        <div
+          onClick={() => onOpen()}
+          className={cn(
+            font,
+            "flex items-center relative justify-center text-[#18181A] text-base px-8 py-4 rounded-full border border-[#18181A] cursor-pointer"
+          )}
+        >
+          {!title ? "ZALOGUJ SIĘ" : title}
         </div>
-      ) : ( */}
-      <div
-        onClick={() => onOpen()}
-        className={cn(
-          font,
-          "flex items-center relative justify-center text-[#18181A] text-base px-8 py-4 rounded-full border border-[#18181A] cursor-pointer"
-        )}
-      >
-        {!title ? "ZALOGUJ SIĘ" : title}
-      </div>
-      {/* )} */}
+      )}
     </Magnetic>
   );
 };
