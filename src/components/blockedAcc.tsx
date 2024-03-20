b145212ade03c6f53aaee78255df52bf1bbe95ac
Lukols-Dev <lukols.dev@gmail.com>
2024-03-20 21:47:17 +0100

    fix text in BlockAccInfo

--- src/components/blockedAcc.tsx
+++ src/components/blockedAcc.tsx
@@ -8,7 +8,7 @@ export const BlockAccInfo = async () => {
   return (
     <div className="z-[20] fixed top-0 left-0 w-screen h-screen bg-red-200/80 flex">
       <div className="m-auto flex">
-        Twoje konto zostało zablokowane. Skontaktój się z Tipey po więcej
+        Twoje konto zostało zablokowane. Skontaktuj się z Tipey po więcej
         informacji.
         <MailTo subject="Zablokowane konto">
           <span className="p-2 border-2">Kontakt</span>
